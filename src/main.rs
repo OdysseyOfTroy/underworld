d7b5d7eb261abb2ff21f7d275d2ae8012d3deb60
OdysseyOfTroy <TroyMatthewsHong@gmail.com>
2026-01-28 16:35:08 +0000

    added basic user input and output with predefined cipher settings

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use simple_user_input::get_input;
+use underworld::{caesar_cipher::Caesar, cipher_traits::CipherTraits, vigenere_cipher::Vigenere};
+
+fn main() {
+  let input: String = get_input("give me a string to encrypt");
+  
+  let ceaser_cipher = Caesar::new(10);
+  let vigenere_cipher = Vigenere::new("Noggle");
+
+  let ceaser_encrypted: String = ceaser_cipher.encrypt(input.as_str());
+  let vigenere_encrypted: String = vigenere_cipher.encrypt(input.as_str());
+
+  println!("your string if ceaser encrypted is: {}", ceaser_encrypted);
+  println!("your string if vigenere encrytped is: {}", vigenere_encrypted);
+}
+
+mod simple_user_input {
+    use std::io;
+    pub fn get_input(prompt: &str) -> String{
+        println!("{}",prompt);
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input.trim().to_string()
+    }
+}
